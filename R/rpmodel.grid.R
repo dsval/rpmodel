506af3f2667b5bab1c4813d579db4635c05bba85
Silwood <Silwood@sphb-xlap-075.sp.ic.ac.uk>
2024-01-30 15:33:55 +0000

    add new phio=fT method

--- R/rpmodel.grid.R
+++ R/rpmodel.grid.R
@@ -8,18 +8,34 @@
 #' @examples
 #' rpmodel.grid()	
 
-rpmodel.grid<-function(tc, vpd, co2, fapar, ppfd, elev = NA,AI=NA ,
-	kphio = ifelse(do_ftemp_kphio, calc_phi0(tc=tc,AI=AI), 0.0817), 
-	beta = 146.0, soilm = 1.0, meanalpha = 1.0, apar_soilm = 0.0, bpar_soilm = 0.685, 
-	c4 = FALSE, method_optci = "prentice14", method_jmaxlim = "wang17", 
-	do_ftemp_kphio = TRUE, do_soilmstress = TRUE,inmem=FALSE,outdir=getwd()){
+rpmodel.grid<-function(tc, vpd, co2, fapar, ppfd, soilm,meanalpha,elev = NA,AI=NA ,
+	beta = 146.0,	do_ftemp_kphio = TRUE, do_soilmstress = TRUE,inmem=FALSE,outdir=getwd()){
 	
+	###########################################################################
+	# 00. Check if parallel computation is required by the user and if the dimensions of the raster objects match
+	###########################################################################
+	on.exit(endCluster())
+	clcheck<-try(getCluster(), silent=TRUE)
+	if(class(clcheck)=="try-error"){
+		# If no cluster is initialized, assume only one core will do the calculations, beginCluster(1) saved me the time of coding serial versions of the functions
+		beginCluster(1,'SOCK')
+		message('Only using one core, use first beginCluster(ncores) if you want to run in parallel!!')
+		
+	}
 	###############################################################################################
 	# 00. create array for results
 	###############################################################################################
 	#rasterOptions(maxmemory=1e7, tmptime = 24, chunksize = 1e6,todisk = FALSE, overwrite=TRUE, tolerance = 0.5)
 	y<-as.numeric(unique(format(getZ(tc),'%Y')))
 	ny <- nlayers(tc)
+	################ arrays for well watered
+	if (!do_soilmstress){
+		soilm=calc(tc,fun= function(x){x/x})
+		meanalpha = soilm[[1]]
+		#soilm=reclassify(tc, cbind(-Inf,+Inf , 1), right=NA);gc()
+	}
+	
+	
 	# gpp gC/m2
 	gpp<-brick(nrows=nrow(elev), ncols=ncol(elev), crs=crs(elev), nl=ny)
 	extent(gpp)<-extent(elev)
@@ -27,7 +43,7 @@ rpmodel.grid<-function(tc, vpd, co2, fapar, ppfd, elev = NA,AI=NA ,
 	# lue mmol C / mol photons
 	lue<-brick(nrows=nrow(elev), ncols=ncol(elev), crs=crs(elev), nl=ny)
 	extent(lue)<-extent(elev)
-	gpp<-setZ(gpp,getZ(tc))
+	lue<-setZ(lue,getZ(tc))
 	# transpiration mm
 	Tr<-brick(nrows=nrow(elev), ncols=ncol(elev), crs=crs(elev), nl=ny)
 	extent(Tr)<-extent(elev)
@@ -58,7 +74,7 @@ rpmodel.grid<-function(tc, vpd, co2, fapar, ppfd, elev = NA,AI=NA ,
 	bs<- blockSize(tc, minblocks=nodes)
 	parallel::clusterEvalQ(cl, library("rpmodel"))
 	#pmodel<-Vectorize(rpmodel,c("tc","vpd","co2","fapar","ppfd","meanalpha","soilm"))
-	parallel::clusterExport(cl, varlist=c("tc","vpd","co2",'elev','fapar',"ppfd","kphio",'beta','soilm','meanalpha','apar_soilm','bpar_soilm','c4','method_optci','method_jmaxlim','do_ftemp_kphio','do_soilmstress','ny','y','bs'),envir=environment()) 
+	parallel::clusterExport(cl, varlist=c("tc","vpd","co2",'elev','fapar',"ppfd",'beta','soilm','AI','meanalpha','do_soilmstress','ny','y','bs','calc_phi0'),envir=environment()) 
 	pb <- pbCreate(bs$n)
 	pb <- txtProgressBar(min=1,max = max(bs$n,2), style = 3)
 	###############################################################################################
@@ -68,6 +84,7 @@ rpmodel.grid<-function(tc, vpd, co2, fapar, ppfd, elev = NA,AI=NA ,
 		tcrow<-split(getValues(tc,bs$row[i], bs$nrows[i]),1:(ncol(elev)*bs$nrows[i]))
 		vpdrow<-split(getValues(vpd,bs$row[i], bs$nrows[i]),1:(ncol(elev)*bs$nrows[i]))
 		elevrow<-split(getValues(elev,bs$row[i], bs$nrows[i]),1:(ncol(elev)*bs$nrows[i]))
+		AIrow<-split(getValues(AI,bs$row[i], bs$nrows[i]),1:(ncol(elev)*bs$nrows[i]))
 		faparrow<-split(getValues(fapar,bs$row[i], bs$nrows[i]),1:(ncol(elev)*bs$nrows[i]))
 		ppfdrow<-split(getValues(ppfd,bs$row[i], bs$nrows[i]),1:(ncol(elev)*bs$nrows[i]))
 		soilmrow<-split(getValues(soilm,bs$row[i], bs$nrows[i]),1:(ncol(elev)*bs$nrows[i]))
@@ -75,12 +92,12 @@ rpmodel.grid<-function(tc, vpd, co2, fapar, ppfd, elev = NA,AI=NA ,
 		co2row<-lapply(1:(ncol(elev)*bs$nrows[i]),function(j) co2)
 		# define function to get transpiration
 		calc_transp<-function(mat,vpd,tc,ppfd ,elev){
-			press<-patm(elev)
+			press<-calc_patm(elev)
 			#assimilation in mmol
 			A_mmol<-(mat$gpp/12.0107)*1000
-			gs_mol<-as.numeric(mat$gs)*press
+			gs_mol<-as.numeric(mat$gs)
 			gs_mol[gs_mol<0]<-NA
-			T_mol<-1.6*gs_mol*(vpd)/press
+			T_mol<-1.6*gs_mol*(vpd)
 			T_mm<-T_mol*(18/density_h2o(tc,press))
 			T_mm[T_mm>1000]<-NA
 			wue<-A_mmol/T_mol
@@ -91,23 +108,28 @@ rpmodel.grid<-function(tc, vpd, co2, fapar, ppfd, elev = NA,AI=NA ,
 			return(result)
 		}
 		
-		# apply vectorized rpmodel						
+		# apply vectorized rpmodel		
+		if(do_ftemp_kphio){
+			kphiorow  =mapply(calc_phi0,tc=tcrow,AI=AIrow,SIMPLIFY = FALSE) 
+		}
+						
+										
+														
+																						
 		result<-mapply(rpmodel, 
 			tc             = tcrow,           # temperature, deg C
 			vpd            = vpdrow,         # Pa,
 			co2            = co2row,          # ppm,
 			elv            = elevrow,        # m.a.s.l.,
-			kphio          = kphio,         # quantum yield efficiency,
-			beta           = beta,         # unit cost ratio a/b,
+			kphio          = kphiorow,         # quantum yield efficiency,
+			beta           = 146,         # unit cost ratio a/b,
 			fapar          = faparrow,      # fraction  ,
 			ppfd           = ppfdrow,      # mol/m2/d month?,
-			method_optci   = method_optci,
-			method_jmaxlim = method_jmaxlim,
 			soilm = soilmrow,
-			meanalpha=alpharow, SIMPLIFY = FALSE)
-		# calc transpiration and wue	
+			meanalpha=alpharow, MoreArgs = list(do_ftemp_kphio = FALSE,do_soilmstress = do_soilmstress),SIMPLIFY = FALSE)
+		# calc transpiration and wue
+		result<-lapply(result,as.data.frame)
 		result<-mapply(calc_transp,result,vpdrow,tcrow,ppfdrow,elevrow, SIMPLIFY = FALSE)
-		gc()
 		return(result)
 	}
 	###############################################################################################
@@ -127,10 +149,10 @@ rpmodel.grid<-function(tc, vpd, co2, fapar, ppfd, elev = NA,AI=NA ,
 			format="CDF",overwrite=TRUE,varname='Tr', varunit='mm',longname='Transpiration',
 			xname="lon", yname="lat", zname="time", zunit=paste("months","since",paste0(y[1]-1,"-",12)))
 		wue<-writeStart(wue,filename=paste0(outdir,"/",y[1],"_",y[length(y)],".",'wue',".","nc"),
-			format="CDF",overwrite=TRUE,varname='wue', varunit='gC/mol',longname='water use efficiency',
+			format="CDF",overwrite=TRUE,varname='wue', varunit='mmol/mol',longname='water use efficiency',
 			xname="lon", yname="lat", zname="time", zunit=paste("months","since",paste0(y[1]-1,"-",12)))
 		lue<-writeStart(lue,filename=paste0(outdir,"/",y[1],"_",y[length(y)],".",'lue',".","nc"),
-			format="CDF",overwrite=TRUE,varname='wue', varunit='gC/mol',longname='light use efficiency',
+			format="CDF",overwrite=TRUE,varname='wue', varunit='mmol/mol',longname='light use efficiency',
 			xname="lon", yname="lat", zname="time", zunit=paste("months","since",paste0(y[1]-1,"-",12)))
 		
 		
